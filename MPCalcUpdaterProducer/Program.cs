48a063d96059e9b7d06d9669a34a5a249b816e66
Gabriel Loureiro <gabrielloureirog@gmail.com>
2025-03-24 22:35:44 -0300

    :hammer: Atualização do Dockerfile

--- MPCalcUpdaterProducer/Program.cs
+++ MPCalcUpdaterProducer/Program.cs
@@ -8,14 +8,24 @@ builder.Services.AddControllers();// Learn more about configuring Swagger/OpenAP
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.UseUrls("http://0.0.0.0:5024");
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseCors("AllowAll");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
--- MPCalcUpdaterProducer/Services/RabbitMqService.cs
+++ MPCalcUpdaterProducer/Services/RabbitMqService.cs
@@ -73,8 +73,7 @@ public class RabbitMqService : IRabbitMqService
             queue: _queueName,
             durable: true,
             exclusive: false,
-            autoDelete: false,
-            arguments: arguments);
+            autoDelete: false);
 
          await channel.QueueBindAsync(
             queue: _queueName,
